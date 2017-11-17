1a112e918b6d4908a14a4f3039534e73b79ee718
Ian <9372700+Minreaux@users.noreply.github.com>
2017-11-16 22:08:18 -0600

    Changed layout of folders
    * Changed layout to better work with ejs partials; required views folder

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
+app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.use(express.static('pages'));
